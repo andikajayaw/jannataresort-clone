927e4ec19fee148b3b321fbb1196a6c6b3944b7d
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-12-02 17:19:11 +0800

    first init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <section id="slider">
+      <SliderComponent />
+    </section>
+    <section id="about">
+      <div style="background: red; width: 100%; height: 100%">hello about</div>
+    </section>
+    <section id="home_accomodation">hello home_accomodation</section>
+    <section id="dinner">hello dinner</section>
+    <section id="spa">hello spa</section>
+    <section id="review">hello review</section>
+    <section id="book">hello book</section>
+    <section id="footer">hello footer</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped></style>
