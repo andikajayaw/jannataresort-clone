2ce3c9a6b6b2138698d773166599a49635474491
LAPTOP-E5999C64\SiNDATA - ANDIKA <andikajayawiguna@gmail.com>
2022-12-05 11:51:26 +0800

    add Spa & Dinner Component, fix accomodation component styling, try error Testimonial Component

---
+++ components/DinnerComponent.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="q-container container-inner">
+    <div class="columns overlap-wrapper type-1">
+      <div class="column block-picture wide">
+        <figure class="picture" style="height: 583px">
+          <div class="rev_clip" style="clip-path: inset(0px); opacity: 1">
+            <img
+              :src="`${model.featured.aws_file_url}/${model.featured.path}/${model.featured.filename.big}`"
+              alt=""
+              data-scroll=""
+              data-scroll-speed="-2"
+              class="clip_image is-inview"
+              style="height: 641.3px; width: auto"
+            />
+          </div>
+        </figure>
+      </div>
+      <div
+        data-scroll=""
+        data-scroll-speed="-2"
+        class="column block-entry bg-white vs-div is-inview"
+      >
+        <div class="inner">
+          <h2 class="title">
+            <span class="q_split"
+              ><span
+                class="q_split_wrap rev_item"
+                style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+                >DINNER WITH</span
+              ></span
+            >
+            <span class="q_split text-green"
+              ><span
+                class="q_split_wrap rev_item"
+                style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+                >JANNATA</span
+              ></span
+            >
+          </h2>
+          <div class="title-line"></div>
+          <div>
+            <p>
+              Amateras Restaurant at Jannata Resort &amp; Spa serves an
+              extensive menu of Western and Asian cuisine that is driven by
+              quality ingredients.
+            </p>
+            <p><br /></p>
+            <p>
+              The restaurant is located next to the lobby and extends out onto
+              an open terrace for an alfresco dining experience. It is elevated
+              over the main swimming pool with tropical views of lush valley
+              greenery in the distance.
+            </p>
+          </div>
+          <div
+            class="rev_item"
+            style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+          >
+            <a class="inline-link q_magnet"> Read More </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      name: 'Component Four',
+      component_name: 'ComponentFour',
+      model: {
+        featured: {
+          id: '5e78388d9dae8c62826241d8',
+          created_at: '2020-03-23 04:18:21',
+          updated_at: '2020-03-23 04:18:21',
+          updatedBy: null,
+          title: null,
+          description: null,
+          size: 195994,
+          type: 'IMG',
+          original: 'e061b86c-217c-4b9f-87ec-bc67b45db7a0.jpg',
+          path: '5e65fe109dae8c14dd70f7e0/pictures/dining/15849371005791',
+          is_aws: true,
+          aws_file_url:
+            'https://vold-chain-hotel.s3-ap-southeast-1.amazonaws.com',
+          filename: {
+            big: 'big/e061b86c-217c-4b9f-87ec-bc67b45db7a0.jpg',
+            raw: 'raw/e061b86c-217c-4b9f-87ec-bc67b45db7a0.jpg',
+            blur: 'blur/e061b86c-217c-4b9f-87ec-bc67b45db7a0.jpg',
+            medium: 'medium/e061b86c-217c-4b9f-87ec-bc67b45db7a0.jpg',
+          },
+        },
+        title_one: 'DINNER WITH',
+        title_two: 'JANNATA',
+        description:
+          '<p>Amateras Restaurant at Jannata Resort &amp; Spa serves an extensive menu of Western and Asian cuisine that is driven by quality ingredients.</p><p><br></p><p>The restaurant is located next to the lobby and extends out onto an open terrace for an alfresco dining experience. It is elevated over the main swimming pool with tropical views of lush valley greenery in the distance.</p>',
+        link: {
+          title: 'Read More',
+          url: 'amateras-dining',
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/SpaComponent.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div
+      class="bg-image is-inview"
+      :style="{
+        backgroundImage: [
+          `url(${model.featured.aws_file_url}/${model.featured.path}/${model.featured.filename.big})`,
+        ],
+      }"
+    ></div>
+    <div class="q-container container-inner">
+      <div class="columns overlap-wrapper type-1">
+        <div class="column block-entry">
+          <div class="inner">
+            <h2
+              class="title rev_item"
+              style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+            >
+              RELAXING WITH JANNATA.
+            </h2>
+            <div class="title-line"></div>
+            <p
+              class="rev_item"
+              style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+            >
+              A sanctuary for the senses, Champaka Spa features a comfortable
+              lounge area and three private treatment rooms for sessions of
+              personal rejuvenation. This charming wellness facility overlooks a
+              tropical valley that descends down to flowing river.
+            </p>
+            <br />
+            <div
+              class="rev_item"
+              style="opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0)"
+            >
+              <a
+                class="inline-link q_magnet"
+                style="transform: translate(0px, 0px)"
+                >Read More</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      name: 'Component Five',
+      component_name: 'ComponentFive',
+
+      model: {
+        title: 'RELAXING WITH JANNATA.',
+        description:
+          'A sanctuary for the senses, Champaka Spa features a comfortable lounge area and three private treatment rooms for sessions of personal rejuvenation. This charming wellness facility overlooks a tropical valley that descends down to flowing river.',
+        featured: {
+          id: '5e6f379e9dae8c6e6bb078fc',
+          created_at: '2020-03-16 08:23:58',
+          updated_at: '2020-03-16 08:23:58',
+          updatedBy: null,
+          title: null,
+          description: null,
+          size: 79004,
+          type: 'IMG',
+          original: '6fe77037-cba3-4b76-81d0-bac03e031898.jpg',
+          path: '5e65fe109dae8c14dd70f7e0/pictures/homepage/15843470372741',
+          is_aws: true,
+          aws_file_url:
+            'https://vold-chain-hotel.s3-ap-southeast-1.amazonaws.com',
+          filename: {
+            big: 'big/6fe77037-cba3-4b76-81d0-bac03e031898.jpg',
+            raw: 'raw/6fe77037-cba3-4b76-81d0-bac03e031898.jpg',
+            blur: 'blur/6fe77037-cba3-4b76-81d0-bac03e031898.jpg',
+            medium: 'medium/6fe77037-cba3-4b76-81d0-bac03e031898.jpg',
+          },
+        },
+        link: {
+          title: 'Read More',
+          url: 'champaka-spa',
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bg-full-image .bg-image {
+  -o-object-fit: cover;
+  object-fit: cover;
+  width: 100%;
+  height: 130%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  opacity: 0.5;
+  background: no-repeat 50%;
+  background-size: cover;
+}
+
+.bg-full-image * {
+  color: #fff;
+}
+</style>
